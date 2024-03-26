ca8639481cff027f6fe18cb754de06bedeeb434d
John Bobst <johnabobst@gmail.com>
2024-03-25 19:25:01 -0700

    Added KeeperResponse dto

---
+++ keeper/src/main/java/com/motw/keeper/dto/KeeperResponse.java
@@ -0,0 +1,16 @@
+package com.motw.keeper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeeperResponse {
+
+    String response;
+
+}
